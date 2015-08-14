ca938335d46e9ab6269801775c6a3ecd76da430f
lzrmihnea <crok91@gmail.com>
2015-08-15 00:10:22 +0300

    Cleanup: removed setupSimParams

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var errors = require('./errors');
-var setupSimInfo = require('./setupSimParams');
 var patients = require('./patients');
 var simulations = require('./simulations');
 var mongoose = require('mongoose');
 var Simulation = require('./../models/simulation.js');
+var Patient = require('./../models/patient.js');
 mongoose.model('Simulation');
+mongoose.model('Patient');
 
 
 module.exports = function (app) {
@@ -13,8 +14,6 @@ module.exports = function (app) {
   app.get('/', function(req, res, next){
       res.render('home.jade');
   })
-
-  setupSimInfo(app);
 
   patients(app);
 
